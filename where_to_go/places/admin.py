3dc995d05502e867e6cc480f57f4585d3763e58d
Sergey Postnikov <ser.post2017@gmail.com>
2023-05-07 14:27:34 +0300

    add Place and Image mode

--- where_to_go/places/admin.py
+++ where_to_go/places/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from places.models import Image, Place
 
-# Register your models here.
+
+class ImageInline(admin.TabularInline):
+    model = Image
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    inlines = [ImageInline]
--- where_to_go/places/models.py
+++ where_to_go/places/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+    img = models.ImageField(blank=True, null=True)
+    place = models.ForeignKey(
+            'Place', 
+            related_name='images', 
+            on_delete=models.CASCADE
+        )
+    order_num = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.order_num}-{self.place.__str__()}'
+
+
+class Place(models.Model):
+    title = models.CharField(max_length=40)
+    description_short = models.TextField(
+        blank=True, 
+        null=True
+    )
+    description_long = models.TextField()
+    lon = models.DecimalField(
+        null=True,
+        max_digits=16,
+        decimal_places=14,
+        verbose_name='Longitude'
+        )
+    lat = models.DecimalField(
+        null=True,
+        max_digits=16,
+        decimal_places=14,
+        verbose_name='Latitude'
+        )
+
+    def __str__(self):
+        return f'{self.title}'
