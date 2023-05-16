6bd4dcc03e7513628f4907d5366943a70c53760f
Sergey Postnikov <ser.post2017@gmail.com>
2023-05-16 13:44:35 +0300

    improve style

--- where_to_go/places/models.py
+++ where_to_go/places/models.py
@@ -5,10 +5,10 @@ from tinymce.models import HTMLField
 class Image(models.Model):
     img = models.ImageField(blank=True, null=True)
     place = models.ForeignKey(
-            'Place', 
-            related_name='images', 
-            on_delete=models.CASCADE
-        )
+        'Place',
+        related_name='images',
+        on_delete=models.CASCADE
+    )
     order_num = models.IntegerField(blank=True, null=True, default=1)
 
     class Meta:
@@ -21,18 +21,18 @@ class Image(models.Model):
 class Place(models.Model):
     title = models.CharField(max_length=40)
     description_short = models.TextField(
-        blank=True, 
+        blank=True,
         null=True
     )
     description_long = HTMLField(blank=True, null=True)
     lon = models.FloatField(
         null=True,
         verbose_name='Longitude'
-        )
+    )
     lat = models.FloatField(
         null=True,
         verbose_name='Latitude'
-        )
+    )
 
     def __str__(self):
         return f'{self.title}'
